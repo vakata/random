e357c2736a7e5640ba4df1fb4bd2576df9c65a54
Ivan Bozhanov <git@vakata.com>
2017-06-02 18:05:55 +0300

    migrated to PHP7 only

--- src/Generator.php
+++ src/Generator.php
@@ -7,92 +7,14 @@ namespace vakata\random;
  */
 class Generator
 {
-    const HIGH = 1;
-    const MEDIUM = 2;
-    const LOW = 4;
-    const ALL = 7;
     /**
      * Generate random bytes.
      * @param  integer $length   amount of bytes to generate
-     * @param  integer $strength bitflag of which sources to use by strength (1-high, 2-medium, 4-low), default to 7
      * @return string            the random bytes
      */
-    public static function bytes($length = 64, $strength = null)
+    public static function bytes(int $length = 64) : string
     {
-        if (!$strength) {
-            $strength = static::ALL;
-        }
-
-        $size = 64;
-        $seeds = [];
-
-        // high
-        if (($strength & static::HIGH)) {
-            // random_bytes() - PHP7
-            if (function_exists('random_bytes')) {
-                $seeds[] = random_bytes($size);
-            }
-
-            // openssl
-            if (function_exists('openssl_random_pseudo_bytes')) {
-                $seeds[] = openssl_random_pseudo_bytes($size);
-            }
-        }
-
-        // medium
-        if (($strength & static::MEDIUM)) {
-            // /dev/urandom (do not use /dev/random as not enough entropy will block reads)
-            $file = '/dev/urandom';
-            if (@file_exists($file) && ($file = @fopen($file, 'rb'))) {
-                if (function_exists('stream_set_read_buffer')) {
-                    stream_set_read_buffer($file, 0);
-                }
-                $seeds[] = fread($file, $size);
-                fclose($file);
-            }
-        }
-
-        // low
-        if (($strength & static::LOW)) {
-            // uniqid
-            $tmp = '';
-            while (strlen($tmp) < $size) {
-                $tmp = uniqid($tmp, true);
-            }
-            $seeds[] = substr($tmp, 0, $size);
-
-            // mt_rand()
-            $tmp = '';
-            for ($i = 0; $i < $size; ++$i) {
-                $tmp .= chr((mt_rand() ^ mt_rand()) % 256);
-            }
-            $seeds[] = $tmp;
-
-            // rand()
-            $tmp = '';
-            for ($i = 0; $i < $size; ++$i) {
-                $tmp .= chr((rand() ^ rand()) % 256);
-            }
-            $seeds[] = $tmp;
-        }
-
-        if (!count($seeds)) {
-            throw new \Exception('No random sources available for specified strength');
-        }
-
-        // combine seeds
-        $result = hash_hmac('sha512', $seeds[0], '', true);
-        for ($i = 1; $i < count($seeds); ++$i) {
-            $result ^= hash_hmac('sha512', $seeds[$i], $seeds[$i - 1], true);
-        }
-        while (strlen($result) < $length) {
-            $result .= static::bytes($length - strlen($result), $strength);
-        }
-        if (strlen($result) > $length) {
-            $result = substr($result, 0, $length);
-        }
-
-        return $result;
+        return random_bytes($length);
     }
     /**
      * Generate a random string.
@@ -101,17 +23,16 @@ class Generator
      * @return string              the random string
      */
     public static function string(
-        $length = 32,
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    ) {
+        int $length = 32,
+        string $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ) : string {
         $rand = static::bytes($length * 2);
-        $lngt = mb_strlen($characters, 'UTF-8');
+        $lngt = iconv_strlen($characters, 'UTF-8');
         $rslt = '';
         for ($i = 0; $i < strlen($rand); ++$i) {
-            $rslt .= mb_substr($characters, ord($rand[$i]) % $lngt, 1, 'UTF-8');
+            $rslt .= iconv_substr($characters, ord($rand[$i]) % $lngt, 1, 'UTF-8');
         }
-
-        return mb_substr($rslt, 0, $length, 'UTF-8');
+        return iconv_substr($rslt, 0, $length, 'UTF-8');
     }
     /**
      * Generate a random number from a range.
@@ -119,7 +40,7 @@ class Generator
      * @param  integer $max the maximum number (defaults to PHP_INT_MAX)
      * @return integer      the random number
      */
-    public static function number($min = 0, $max = PHP_INT_MAX)
+    public static function number(int $min = 0, int $max = PHP_INT_MAX) : int
     {
         if ($max === $min) {
             return $max;
@@ -129,9 +50,9 @@ class Generator
 
         return $min + abs($rand % ($max - $min + 1));
     }
-    public static function uuid()
+    public static function uuid() : string
     {
-        $hash = bin2hex(static::bytes(16, static::HIGH | static::MEDIUM));
+        $hash = bin2hex(static::bytes(16));
         
         $timeHiVersion = substr($hash, 12, 4);
         $timeHiVersion = hexdec($timeHiVersion) & 0x0fff;
